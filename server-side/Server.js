b18a2acc98ee096a256f042324c6029016063e28
GagandeepV <gagandeep.veerabhadregowda@meredith.com>
2022-07-24 03:51:16 +0530

    Routes with functions

--- server-side/Server.js
+++ server-side/Server.js
@@ -1,16 +1,21 @@
 import express from 'express'
+import cors from 'cors'
+import router from './routes/ServeElastic.js'
 
 const app = express()
+const bodyParser = express.json()
 const PORT = 1234
 
-app.get('/', (req,res)=>{
+app.use(cors())
+app.use(bodyParser)
+
+
+app.get('/', (req, res) => {
     res.send('Port - 1234')
 })
 
-app.get('/api', (req,res)=>{
-    res.json({a:1,b:2})
-})
+app.use('/backend', router)
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`The server is up and running on port ${PORT}`);
 })
---
+++ server-side/routes/ServeElastic.js
@@ -0,0 +1,83 @@
+import express from 'express'
+
+const router = express.Router()
+
+let data = []
+
+router.get('/', (req, res) => {
+    res.json(data)
+})
+
+router
+    .route('/:id')
+    .get((req, res) => {
+        const { params: { id: reqID } } = req
+        data = [...data.filter(({ id: dataID }) => dataID === Number.parseInt(reqID))]
+        res.json(data)
+    })
+    .post((req, res) => {
+        const { body: { id, ...rest } } = req
+        data = [...data, { id, ...rest }]
+        res.send(`Added data with id ${id}`)
+    })
+    .put((req, res) => {
+        const { params: { id: reqID } } = req
+        data = [...data.map(({ id: dataID, ...rest }) => dataID === Number.parseInt(reqID) ? ({ ...m, ...rest }) : ({ id: dataID, ...rest }))]
+        res.send(`Modified data with id ${reqID}`)
+    })
+    .delete((req, res) => {
+        const { params: { id: reqID } } = req
+        data = [...data.filter(({ id: dataID }) => Number.parseInt(reqID) !== dataID)]  
+        res.send(`Deleted data with id ${reqID}`)
+    })
+
+
+export default router
+
+
+//old format or methods -- just for reference
+// router.get('/', (req, res) => {
+//     res.json(data)
+// })
+
+// router.route('/:id')
+//     .get((req, res) => {
+//         const { id } = req.params
+//         let index = data.findIndex((f) => f.id === Number.parseInt(id))
+//         if (index >= 0) res.json(data[index])
+//         else {
+//             res.status(404)
+//             res.end()
+//         }
+//     })
+//     .post((req, res) => {
+//         const { id, input, status } = req.body
+//         const objectToPush = { id: id, input: input, status: status }
+//         data.push(objectToPush)
+//         res.json(objectToPush)
+//     })
+//     .put((req, res) => {
+//         const { id } = req.params
+//         const { input, status } = req.body
+//         let index = data.findIndex((f) => f.id === Number.parseInt(id))
+//         if (index >= 0) {
+//             let objectFromData = data[index]
+//             objectFromData.input = input
+//             objectFromData.status = status
+//             res.json(objectFromData)
+//         } else {
+//             res.status(404)
+//             res.end()
+//         }
+//     })
+//     .delete((req, res) => {
+//         const { id } = req.params
+//         let index = data.findIndex((f) => f.id === Number.parseInt(id))
+//         if (index >= 0) {
+//             data.splice(index, 1)
+//             res.send(`Deleted todo with id ${id}`)
+//         } else {
+//             res.status(404)
+//             res.end()
+//         }
+//     })
